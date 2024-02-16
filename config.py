1e8b9a97eb295fb74d1eb6548f909c4aef946545
Islam <it.kaktus312@gmail.com>
2024-02-16 21:18:50 +0600

    Creating an Administrator Privilege Check

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ BOT_TOKEN = config('TOKEN')
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot=bot, storage=storage)
 DESTINATION = config('DESTINATION')
+ADMIN_ID = config("ADMIN_ID")
+SECRET_WORD = config("SECRET_WORD")
+
--- handlers/call_back.py
+++ handlers/call_back.py
@@ -1,7 +1,5 @@
-import sqlite3
-
 from aiogram import types, Dispatcher
-from config import bot
+from config import bot, ADMIN_ID, SECRET_WORD
 from database.sql_commands import DataBase
 from keyboards.inline_buttons import questionnaire_keyboard
 
@@ -39,6 +37,20 @@ async def save_answer(call: types.CallbackQuery):
     print(call.data)
 
 
+async def admin_call(message: types.Message):
+    if message.from_user.id == int(ADMIN_ID):
+        await message.delete()
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text='You are admin 🚀'
+        )
+    else:
+        await bot.send_message(
+            chat_id=message.from_user.id,
+            text='You are not admin ⛔️'
+        )
+
+
 def register_call_back_handlers(dp: Dispatcher):
     dp.register_callback_query_handler(start_questionnaire_call,
                                        lambda call: call.data == 'start_questionnaire')
@@ -46,3 +58,5 @@ def register_call_back_handlers(dp: Dispatcher):
                                        lambda call: call.data == 'first')
     dp.register_callback_query_handler(second_card,
                                        lambda call: call.data == 'second')
+    dp.register_message_handler(admin_call,
+                                lambda word: SECRET_WORD in word.text)
